8c3c95f2bd1372c695dfde2b43dcc265ca12e22a
git-chad <tobimocc@gmail.com>
2023-11-02 10:38:26 -0300

    added placeholder to select inputs

--- app/components/dashboard-module/surveys/inputType.jsx
+++ app/components/dashboard-module/surveys/inputType.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const resizeTextarea = (event) => {
-    event.target.style.height = 'auto';
-    event.target.style.height = `${event.target.scrollHeight}px`;
+  event.target.style.height = "auto";
+  event.target.style.height = `${event.target.scrollHeight}px`;
 };
 
 const InputType = ({ type, options, register, name }) => {
   switch (type) {
     case "text":
       return (
-        <textarea 
+        <textarea
           {...register(name)}
           className="resize-none overflow-hidden transition-all block py-1.5 px-3 border border-gray-300 rounded-md shadow-sm text-gray-900 focus:outline-none focus:ring-2 focus:ring-bedeblue sm:text-sm"
           onInput={resizeTextarea}
@@ -19,11 +19,19 @@ const InputType = ({ type, options, register, name }) => {
     case "select":
       return (
         <div className="relative mt-2">
-          <select 
-          {...register(name, { required: 'This field is required.' })} 
-          className="block w-full py-1.5 pl-3 pr-10 text-gray-900 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-bedeblue sm:text-sm">
+          <select
+            {...register(name, { required: "This field is required." })}
+            className="block w-full py-1.5 pl-3 pr-10 text-gray-900 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-bedeblue sm:text-sm"
+          >
+            <option value="" disabled selected>
+              Please, select an option
+            </option>
             {options.map((option, index) => (
-              <option key={index} value={option} className="text-gray-900 cursor-pointer">
+              <option
+                key={index}
+                value={option}
+                className="text-gray-900 cursor-pointer"
+              >
                 {option}
               </option>
             ))}
