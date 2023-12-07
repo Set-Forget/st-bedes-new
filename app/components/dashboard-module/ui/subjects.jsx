d0faf649b949288b12eff52e72de6248129dc06b
git-chad <tobimocc@gmail.com>
2023-12-07 14:51:19 -0300

    fix(dev): fixed bug where surveys with no data were appearing in the dashboard

--- app/components/dashboard-module/ui/subjects.jsx
+++ app/components/dashboard-module/ui/subjects.jsx
@@ -25,13 +25,13 @@ export const Subjects = ({ surveys, onSelect, filterType }) => {
   }, [surveys, filterType]);
 
   const calculateTeacherSurveys = (subject) => {
-    const relatedSurveys = surveys.filter(s => s.subject_name === subject);
+    const relatedSurveys = surveys.filter((s) => s.subject_name === subject);
     const surveysByTeacher = relatedSurveys.reduce((acc, current) => {
       const { teacher_id, is_answered } = current;
       if (!acc[teacher_id]) {
         acc[teacher_id] = {
           completedQuestions: 0,
-          totalQuestions: 0
+          totalQuestions: 0,
         };
       }
       acc[teacher_id].totalQuestions += 1;
@@ -42,7 +42,7 @@ export const Subjects = ({ surveys, onSelect, filterType }) => {
     }, {});
 
     const completedTeacherSurveys = Object.values(surveysByTeacher).filter(
-      teacher => teacher.completedQuestions === teacher.totalQuestions
+      (teacher) => teacher.completedQuestions === teacher.totalQuestions
     ).length;
 
     const totalTeachers = Object.keys(surveysByTeacher).length;
@@ -51,7 +51,8 @@ export const Subjects = ({ surveys, onSelect, filterType }) => {
   };
 
   const countCompletedSurveys = (subject) => {
-    const { completedTeacherSurveys, totalTeachers } = calculateTeacherSurveys(subject);
+    const { completedTeacherSurveys, totalTeachers } =
+      calculateTeacherSurveys(subject);
     return `${completedTeacherSurveys}/${totalTeachers}`;
   };
 
@@ -62,52 +63,56 @@ export const Subjects = ({ surveys, onSelect, filterType }) => {
 
   return (
     <ul role="list" className="divide-y divide-gray-200">
-      {subjects.map((subject, index) => (
-        <li
-          key={index}
-          style={{ animationDelay: `${index * 0.125}s` }}
-          className="animate-fadeIn flex items-center justify-between sm:gap-x-96 gap-x-36 py-5 opacity-0"
-        >
-          <div className="min-w-0">
-            <div className="flex flex-col items-start gap-x-3">
-              <p className="text-sm font-semibold leading-6 text-gray-900">
-                {subject}
-              </p>
-              <div className="flex flex-col">
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  School subject
+      {subjects.map((subject, index) => {
+        if (!subject) return null;
+
+        return (
+          <li
+            key={index}
+            style={{ animationDelay: `${index * 0.125}s` }}
+            className="animate-fadeIn flex items-center justify-between sm:gap-x-96 gap-x-36 py-5 opacity-0"
+          >
+            <div className="min-w-0">
+              <div className="flex flex-col items-start gap-x-3">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {subject}
                 </p>
-                <div className="flex space-x-2 items-center">
-                  {isSubjectSurveyCompleted(subject) ? (
-                    <span className="bg-green-100 text-green-700 px-2 py-1 rounded text-xs">
-                      Completed
-                    </span>
-                  ) : (
-                    <>
-                      <span className="bg-yellow-100 text-yellow-700 px-2 py-1 rounded text-xs text-center">
-                        Pending
+                <div className="flex flex-col">
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    School subject
+                  </p>
+                  <div className="flex space-x-2 items-center">
+                    {isSubjectSurveyCompleted(subject) ? (
+                      <span className="bg-green-100 text-green-700 px-2 py-1 rounded text-xs">
+                        Completed
                       </span>
-                      <span className="text-xs leading-5 text-gray-500">
-                        {countCompletedSurveys(subject)}
-                      </span>
-                    </>
-                  )}
+                    ) : (
+                      <>
+                        <span className="bg-yellow-100 text-yellow-700 px-2 py-1 rounded text-xs text-center">
+                          Pending
+                        </span>
+                        <span className="text-xs leading-5 text-gray-500">
+                          {countCompletedSurveys(subject)}
+                        </span>
+                      </>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-none items-center gap-x-4">
-            <button
-              className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset 
+            <div className="flex flex-none items-center gap-x-4">
+              <button
+                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset 
               ring-gray-300  hover:bg-gray-50 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-300"
-              onClick={() => onSelect(subject)}
-              disabled={isSubjectSurveyCompleted(subject)}
-            >
-              Choose<span className="sr-only">, {subject}</span>
-            </button>
-          </div>
-        </li>
-      ))}
+                onClick={() => onSelect(subject)}
+                disabled={isSubjectSurveyCompleted(subject)}
+              >
+                Choose<span className="sr-only">, {subject}</span>
+              </button>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 };
